4e08903a0d8e08886bcd7809bc2f2deff29a4021
Larskerstens <larskerstens10@gmail.com>
2021-12-15 17:56:50 +0100

    pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
+import { ThemeProvider, theme, CSSReset, Box } from "@chakra-ui/react";
+import Layout from "../components/Layout";
+import "../css/style.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <CSSReset />
+        <Layout>
+          <Box id='fullpage'>
+            <Component {...pageProps} />
+          </Box>
+        </Layout>
+      </ThemeProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
